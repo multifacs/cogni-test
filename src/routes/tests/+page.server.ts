64d7739d2189cae1ecae0f138aa1384310265600
multifacs <kiwi0tails@gmail.com>
2025-03-19 21:07:18 +0300

    feat: test page

--- src/routes/tests/+page.server.ts
+++ src/routes/tests/+page.server.ts
@@ -1,7 +1,15 @@
 
 export function load({ cookies }) {
     const user = cookies.get('user');
+
+    const tests = {
+        'Тест Струпа': '/tests/stroop',
+        'Тест 2': '/tests/stroop',
+        'Тест 3': '/tests/stroop',
+    };
+
     return {
+        tests,
         user
     };
 }
--- src/routes/tests/+page.svelte
+++ src/routes/tests/+page.svelte
@@ -1,14 +1,41 @@
-<script>
+<script lang="ts">
 	import { userStore } from '$lib/stores';
 	import { onMount } from 'svelte';
 
 	let { data } = $props();
 
 	onMount(() => {
-		userStore.set(data.user || "");
+		userStore.set(data.user || '');
+		console.log(Object.entries(data.tests))
 	});
 </script>
 
 <h1>Тесты</h1>
+<div class="test-container">
+	{#each Object.entries(data.tests) as [test, link]}
+		<a href={link}>{test}</a>
+	{/each}
+</div>
 
-<a href="/tests/stroop">Тест Струпа</a>
+<style>
+	.test-container {
+		background-color: #4c4c4c;
+		padding: 20px;
+		border-radius: 10px;
+		box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+		width: 300px;
+		margin: 0 auto;
+		display: flex;
+		flex-direction: column;
+		gap: 15px;
+	}
+
+	.test-container a {
+		background-color: #dcdcdc;
+		display: block;
+		box-sizing: border-box;
+		padding: 20px 10px;
+		border-radius: 10px;
+		text-decoration: none;
+	}
+</style>
