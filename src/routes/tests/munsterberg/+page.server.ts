5c8c0829c13fdbcafb84aa338688ac5875405f78
multifacs <kiwi0tails@gmail.com>
2025-03-21 23:04:18 +0300

    feat: goshas secret

--- src/routes/tests/munsterberg/+page.server.ts
+++ src/routes/tests/munsterberg/+page.server.ts
@@ -1,11 +1,20 @@
-export async function load({ fetch }) {
+export async function load({ fetch, cookies }) {
     const response = await fetch('/words'); // Путь к файлу
     const text = await response.text();
-    const words = text
+    let words = text
         .split('\n')
         .map(x => x.split('\t')[0])
-        .filter(x => x.length <= 7);
-    console.log(words);
+        .filter(x => x.length <= 9);
+
+    const gosha = cookies.get('gosha');
+    if (gosha) {
+        const response2 = await fetch('/goshas_dict.txt');
+        const text2 = await response2.text();
+        words = text2
+            .split('\n')
+            .map(x => x.toLowerCase().trim())
+            .filter(x => x.length <= 7);
+    }
     return {
         words
     }
