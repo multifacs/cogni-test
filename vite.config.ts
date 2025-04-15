85603323acc9bfe0d687df0ff3e8b33ce07856f1
multifacs <kiwi0tails@gmail.com>
2025-04-15 19:47:32 +0300

    feat: remove console log on prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		tailwindcss()
-	],
+	esbuild: {
+		drop: process.env.MODE == 'PROD' ? ['console', 'debugger'] : []
+	},
+	plugins: [sveltekit(), tailwindcss()],
 	server: {
 		// allowedHosts: true,
-	  }
+	}
 });
