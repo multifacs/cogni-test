daac2d8cc3a5d8231017d5f74f2c2b82f6d285c4
multifacs <kiwi0tails@gmail.com>
2025-04-16 03:41:00 +0300

    misc: buttons and stuff

--- src/routes/tests/[slug]/about/+page.svelte
+++ src/routes/tests/[slug]/about/+page.svelte
@@ -26,8 +26,8 @@
 		class="	controls
   flex items-center justify-center gap-2.5"
 	>
-		<Button color="green" goto={`/tests/${slug}/playground`}>Начать</Button>
 		<Button color="red" goto="/">Назад</Button>
+		<Button color="green" goto={`/tests/${slug}/playground`}>Начать</Button>
 		<Button color="blue" goto={`/tests/${slug}/results`}>История</Button>
 	</div>
 {:else}
