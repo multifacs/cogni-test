c2936887b0c61248729d0676f6a6dbb51f6ba754
multifacs <kiwi0tails@gmail.com>
2025-04-20 14:27:41 +0300

    feat: stroop new result chart

--- src/lib/tests/stroop/Results.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-	import ResultsChart from "$lib/components/charts/ResultsChart.svelte";
-</script>
-
-<ResultsChart stages={3} results={game.getResults()} xtitle="Нажатие" ytitle="Время реакции (мс)"
-></ResultsChart>
