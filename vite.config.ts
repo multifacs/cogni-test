1a9084eb635571a3b8ecc234e526e9486071e669
multifacs <kiwi0tails@gmail.com>
2025-06-30 04:02:22 +0300

    feat: misc

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
-import mkcert from'vite-plugin-mkcert'
+import mkcert from 'vite-plugin-mkcert';
+import devtoolsJson from 'vite-plugin-devtools-json';
 
 export default defineConfig({
 	esbuild: {
 		drop: process.env.MODE == 'PROD' ? ['console', 'debugger'] : []
 	},
-	plugins: [sveltekit(), tailwindcss(), mkcert()],
+	plugins: [sveltekit(), tailwindcss(), mkcert(), devtoolsJson()],
 	define: {
 		'process.env.NODE_ENV': '"production"'
 	}
