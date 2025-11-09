8b24752375bc1daeecd6b185d4243121e29c8a60
multifacs <kiwi0tails@gmail.com>
2025-11-10 02:50:57 +0300

    action fixes

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -7,30 +7,6 @@
 	let { data, children }: { data: LayoutData; children: Snippet } = $props();
 	onMount(async () => {
 		userStore.set(data.user);
-
-		if (data.MODE == 'PROD' && 'serviceWorker' in navigator && 'PushManager' in window) {
-			const registration = await navigator.serviceWorker.register('/service-worker.js');
-
-			// Проверяем разрешение
-			let permission = Notification.permission;
-			if (permission === 'default') {
-				permission = await Notification.requestPermission();
-			}
-			if (permission !== 'granted') return;
-
-			// Подписываемся
-			const subscription = await registration.pushManager.subscribe({
-				userVisibleOnly: true,
-				applicationServerKey: data.VAPID_PUBLIC_KEY
-			});
-
-			// Отправляем подписку на сервер
-			await fetch('/api/subscribe', {
-				method: 'POST',
-				headers: { 'Content-Type': 'application/json' },
-				body: JSON.stringify(subscription)
-			});
-		}
 	});
 </script>
 
