daaf5fca5466d5c2671d398450727e3d2ab0f058
Roddyck <rodion.ivashchenko2003@gmail.com>
2025-11-13 15:52:00 +0300

    feat: add subscription management sub/unsub to profile page

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -7,20 +7,21 @@
 	import Button from '$lib/components/ui/Button.svelte';
 	import Modal from '$lib/components/ui/Modal.svelte';
 	import NavBar from '$lib/components/ui/NavBar.svelte';
+	import { isSubscribed } from '$lib/utils/push';
 
-    let subscription: PushSubscription | null = $state(null);
-    let isSubscribed = $state(false);
-    let showModal = $state(false);
+	let subscription: PushSubscription | null = $state(null);
+	let subscribed = $state(false);
+	let showModal = $state(false);
 
 	let { data, children }: { data: LayoutData; children: Snippet } = $props();
 
 	onMount(async () => {
-        userStore.set(data.user);
+		userStore.set(data.user);
 
-        subscription = await pushService.getSubscription();
-        isSubscribed = !!subscription;
-        showModal = !subscription;
-        console.log('showModal:', showModal);
+		subscribed = await isSubscribed();
+		subscription = await pushService.getSubscription();
+		showModal = !subscribed;
+		console.log('showModal:', showModal);
 	});
 
 	async function subscribe() {
@@ -31,102 +32,38 @@
 
 		try {
 			await pushService.subscribe();
-			isSubscribed = true;
+			subscribed = true;
 			showModal = false;
 			console.log('Subscribed successfully');
 		} catch (error) {
 			console.error('Failed to subscribe:', error);
 		}
 	}
-
-	async function unsubscribe() {
-		try {
-			await pushService.unsubscribe();
-			subscription = null;
-			isSubscribed = false;
-			console.log('Unsubscribed successfully');
-		} catch (error) {
-			console.error('Failed to unsubscribe:', error);
-		}
-	}
-
-	async function sendTestNotification() {
-		if (!subscription) return;
-
-		try {
-			await fetch('/api/push/send', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json'
-				},
-				body: JSON.stringify({
-					subscription,
-					payload: {
-						title: 'Test Notification',
-						body: 'This is a test push notification!',
-						icon: '/favicon.png'
-					}
-				})
-			});
-		} catch (error) {
-			console.error('Failed to send notification:', error);
-		}
-	}
-
-	async function sendAllTestNotification() {
-		if (!subscription) return;
-
-		try {
-			await fetch('/api/push/send-to-all', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json'
-				},
-				body: JSON.stringify({
-					subscription,
-					payload: {
-						title: 'Test Notification',
-						body: 'This is a test push notification for all!',
-						icon: '/favicon.png'
-					}
-				})
-			});
-		} catch (error) {
-			console.error('Failed to send notification:', error);
-		}
-	}
 </script>
 
 <div>
-	<h1>Push Notifications Demo</h1>
-
-	{#if showModal}
-		<Modal bind:showModal>
-			{#snippet header()}
-				<h2 class="text-2xl text-white">Подпишитесь на пуш-уведомления</h2>
-			{/snippet}
-			<div class="flex flex-col gap-4">
-				<p class="text-white">
-					Для корректной работы некоторых функций требуется подписка на уведомления.
-					Например, мы сможем отправлять вам напоминания о прохождении тестов.
-				</p>
-				<p class="text-white">Для подписки достаточно нажать зелёную кнопочку и всё</p>
-				<Button color="green" onclick={subscribe}>Подписаться</Button>
-				<Button color="red" onclick={() => (showModal = false)}>Нет, спасибо</Button>
-			</div>
-		</Modal>
-	{/if}
-
-	{#if isSubscribed}
-		<p>✅ You are subscribed to notifications</p>
-		<Button color="red" onclick={unsubscribe}>Unsubscribe</Button>
-		<Button color="blue" onclick={sendTestNotification}>Send Test Notification</Button>
-		<Button color="blue" onclick={sendAllTestNotification}>Send All Test Notification</Button>
-	{:else}
-		<p>❌ You are not subscribed to notifications</p>
-		<Button color="green" onclick={subscribe}>Subscribe to Notifications</Button>
-	{/if}
+{#if showModal}
+	<Modal bind:showModal>
+		{#snippet header()}
+			<h2 class="text-2xl text-white">Подпишитесь на пуш-уведомления</h2>
+		{/snippet}
+		<div class="flex flex-col gap-4">
+			<p class="text-white">
+				Для корректной работы некоторых функций требуется подписка на уведомления. Например,
+				мы сможем отправлять вам напоминания о прохождении тестов.
+			</p>
+			<p class="text-white">Для подписки достаточно нажать зелёную кнопочку.</p>
+			<p class="text-white">
+				Вы всегда сможете подписаться или отписаться от push-уведомлений в любое время на
+				странице профиля.
+			</p>
+			<Button color="green" onclick={subscribe}>Подписаться</Button>
+			<Button color="red" onclick={() => (showModal = false)}>Нет, спасибо</Button>
+		</div>
+	</Modal>
+{/if}
 </div>
+
 <div class="flex h-dvh w-full flex-col items-center sm:w-2xl">
 	<div
 		class="flex w-full flex-col items-center overflow-auto"
