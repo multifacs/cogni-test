844e4d2b12bae45e1abcb703e7fab59593475349
Roddyck <rodion.ivashchenko2003@gmail.com>
2025-11-19 20:53:11 +0300

    add redirect to playground if has active session

--- src/routes/(app)/exercises/[slug]/about/+page.server.ts
+++ src/routes/(app)/exercises/[slug]/about/+page.server.ts
@@ -1,7 +1,21 @@
-import { error } from '@sveltejs/kit';
+import { error, redirect } from '@sveltejs/kit';
+import { hasActiveSession } from '$lib/server/db/controllers/wordMorphingSessions';
+import type { PageServerLoad } from './$types';
 
-export async function load({ params }) {
+export const load: PageServerLoad = async ({ cookies, params }) => {
+    const userId = cookies.get('user_id');
 	const slug = params.slug;
+
+    if (slug === 'word-morphing') {
+        if (!userId) {
+            error(401, 'Missing userId cookie');
+        }
+
+        const hasSession = await hasActiveSession(userId);
+        if (hasSession) {
+            redirect(303, `/exercises/${slug}/playground`);
+        }
+    }
 
 	try {
 		await import(`$lib/exercises/${slug}/About.svelte`);
