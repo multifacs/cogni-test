c098e6cf257670e5da3db8dbd42a2f995a1412aa
MultiFaCs <kiwi0tails@gmail.com>
2026-05-17 20:27:39 +0300

    feat: admin panel rework

--- apps/cogni-test/src/routes/(app)/admin/login/+page.svelte
+++ apps/cogni-test/src/routes/(app)/admin/login/+page.svelte
@@ -6,16 +6,32 @@
 	let { form } = $props();
 </script>
 
-<form
-	class="mx-auto flex max-w-sm flex-col gap-4 rounded-3xl bg-gray-700 p-6 text-white shadow-xl"
-	method="POST"
-	action="?/login"
-	use:enhance
->
-	<h2>Please login to continue</h2>
-	{#if form?.incorrect}
-		<p class="text-red-500">Incorrect password</p>
-	{/if}
-	<TextInput plain type="password" name="password" id="password" placeholder="Enter password..." required />
-	<Button type="submit" color="green">Login</Button>
-</form>
+<section class="banner">
+	<h1 class="text-3xl font-bold">Admin page</h1>
+</section>
+
+<main class="main flex flex-col items-center justify-center gap-4">
+	<form
+		class="mx-auto flex max-w-sm flex-col gap-4 rounded-3xl bg-gray-700 p-6 text-white shadow-xl"
+		method="POST"
+		action="?/login"
+		use:enhance
+	>
+		<h2>Please login to continue</h2>
+		{#if form?.incorrect}
+			<p class="text-red-500">Incorrect password</p>
+		{/if}
+		<TextInput
+			plain
+			type="password"
+			name="password"
+			id="password"
+			placeholder="Enter password..."
+			required
+		/>
+		<Button type="submit" color="green">Login</Button>
+	</form>
+</main>
+<section class="low-content flex items-center justify-center">
+	<p class="max-w-md text-center text-lg max-md:text-sm">Вход в админку</p>
+</section>
