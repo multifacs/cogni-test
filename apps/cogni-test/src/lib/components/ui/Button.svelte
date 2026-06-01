35a6aba97992403a236336f2ee90fd4b93e5fc25
MultiFaCs <kiwi0tails@gmail.com>
2026-06-01 04:21:21 +0300

    style: button, added white bg

--- apps/cogni-test/src/lib/components/ui/Button.svelte
+++ apps/cogni-test/src/lib/components/ui/Button.svelte
@@ -21,7 +21,8 @@
 		| 'sky'
 		| 'violet'
 		| 'fuchsia'
-		| 'rose';
+		| 'rose'
+		| 'cream';
 	type OnclickType = MouseEventHandler<HTMLButtonElement> | null | undefined;
 	type TypeType = 'button' | 'submit' | 'reset' | null | undefined;
 	let {
@@ -58,6 +59,7 @@
 			hover: string;
 			ring: string;
 			offset: string;
+			text: string;
 		};
 	};
 
@@ -66,109 +68,134 @@
 			bg: 'bg-red-700',
 			hover: 'hover:bg-red-800',
 			ring: 'focus:ring-red-600',
-			offset: 'focus:ring-offset-red-300'
+			offset: 'focus:ring-offset-red-300',
+			text: 'text-white'
 		},
 		blue: {
 			bg: 'bg-blue-700',
 			hover: 'hover:bg-blue-800',
 			ring: 'focus:ring-blue-600',
-			offset: 'focus:ring-offset-blue-300'
+			offset: 'focus:ring-offset-blue-300',
+			text: 'text-white'
 		},
 		green: {
 			bg: 'bg-green-700',
 			hover: 'hover:bg-green-800',
 			ring: 'focus:ring-green-600',
-			offset: 'focus:ring-offset-green-300'
+			offset: 'focus:ring-offset-green-300',
+			text: 'text-white'
 		},
 		gray: {
 			bg: 'bg-gray-700',
 			hover: 'hover:bg-gray-800',
 			ring: 'focus:ring-gray-600',
-			offset: 'focus:ring-offset-gray-300'
+			offset: 'focus:ring-offset-gray-300',
+			text: 'text-white'
 		},
 		yellow: {
 			bg: 'bg-yellow-600',
 			hover: 'hover:bg-yellow-700',
 			ring: 'focus:ring-yellow-500',
-			offset: 'focus:ring-offset-yellow-300'
+			offset: 'focus:ring-offset-yellow-300',
+			text: 'text-white'
 		},
 		purple: {
 			bg: 'bg-purple-700',
 			hover: 'hover:bg-purple-800',
 			ring: 'focus:ring-purple-600',
-			offset: 'focus:ring-offset-purple-300'
+			offset: 'focus:ring-offset-purple-300',
+			text: 'text-white'
 		},
 		pink: {
 			bg: 'bg-pink-700',
 			hover: 'hover:bg-pink-800',
 			ring: 'focus:ring-pink-600',
-			offset: 'focus:ring-offset-pink-300'
+			offset: 'focus:ring-offset-pink-300',
+			text: 'text-white'
 		},
 		indigo: {
 			bg: 'bg-indigo-700',
 			hover: 'hover:bg-indigo-800',
 			ring: 'focus:ring-indigo-600',
-			offset: 'focus:ring-offset-indigo-300'
+			offset: 'focus:ring-offset-indigo-300',
+			text: 'text-white'
 		},
 		teal: {
 			bg: 'bg-teal-700',
 			hover: 'hover:bg-teal-800',
 			ring: 'focus:ring-teal-600',
-			offset: 'focus:ring-offset-teal-300'
+			offset: 'focus:ring-offset-teal-300',
+			text: 'text-white'
 		},
 		orange: {
 			bg: 'bg-orange-700',
 			hover: 'hover:bg-orange-800',
 			ring: 'focus:ring-orange-600',
-			offset: 'focus:ring-offset-orange-300'
+			offset: 'focus:ring-offset-orange-300',
+			text: 'text-white'
 		},
 		cyan: {
 			bg: 'bg-cyan-700',
 			hover: 'hover:bg-cyan-800',
 			ring: 'focus:ring-cyan-600',
-			offset: 'focus:ring-offset-cyan-300'
+			offset: 'focus:ring-offset-cyan-300',
+			text: 'text-white'
 		},
 		lime: {
 			bg: 'bg-lime-700',
 			hover: 'hover:bg-lime-800',
 			ring: 'focus:ring-lime-600',
-			offset: 'focus:ring-offset-lime-300'
+			offset: 'focus:ring-offset-lime-300',
+			text: 'text-white'
 		},
 		amber: {
 			bg: 'bg-amber-700',
 			hover: 'hover:bg-amber-800',
 			ring: 'focus:ring-amber-600',
-			offset: 'focus:ring-offset-amber-300'
+			offset: 'focus:ring-offset-amber-300',
+			text: 'text-white'
 		},
 		emerald: {
 			bg: 'bg-emerald-700',
 			hover: 'hover:bg-emerald-800',
 			ring: 'focus:ring-emerald-600',
-			offset: 'focus:ring-offset-emerald-300'
+			offset: 'focus:ring-offset-emerald-300',
+			text: 'text-white'
 		},
 		sky: {
 			bg: 'bg-sky-700',
 			hover: 'hover:bg-sky-800',
 			ring: 'focus:ring-sky-600',
-			offset: 'focus:ring-offset-sky-300'
+			offset: 'focus:ring-offset-sky-300',
+			text: 'text-white'
 		},
 		violet: {
 			bg: 'bg-violet-700',
 			hover: 'hover:bg-violet-800',
 			ring: 'focus:ring-violet-600',
-			offset: 'focus:ring-offset-violet-300'
+			offset: 'focus:ring-offset-violet-300',
+			text: 'text-white'
 		},
 		fuchsia: {
 			bg: 'bg-fuchsia-700',
 			hover: 'hover:bg-fuchsia-800',
 			ring: 'focus:ring-fuchsia-600',
-			offset: 'focus:ring-offset-fuchsia-300'
+			offset: 'focus:ring-offset-fuchsia-300',
+			text: 'text-white'
 		},
 		rose: {
 			bg: 'bg-rose-700',
 			hover: 'hover:bg-rose-800',
 			ring: 'focus:ring-rose-600',
-			offset: 'focus:ring-offset-rose-300'
+			offset: 'focus:ring-offset-rose-300',
+			text: 'text-white'
+		},
+		cream: {
+			bg: 'bg-stone-50',
+			hover: 'hover:bg-stone-100',
+			ring: 'focus:ring-stone-200',
+			offset: 'focus:ring-offset-stone-100',
+			text: 'text-stone-900'
 		}
 	};
 </script>
@@ -182,7 +209,7 @@
 	active:ring-white/50
 	${colorClasses[disabled ? 'gray' : color].bg}
 	touch-none
-	text-white
+	${colorClasses[color].text}
 	transition
 	duration-200
 	ease-in
