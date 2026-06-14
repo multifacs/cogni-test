587ba9d6155d415c619cb50e9e12107ce5f43964
MultiFaCs <kiwi0tails@gmail.com>
2026-06-14 19:49:24 +0300

    style: centered results list in tests

--- src/routes/(app)/tests/[slug]/results/+page.svelte
+++ src/routes/(app)/tests/[slug]/results/+page.svelte
@@ -56,54 +56,50 @@
 	});
 </script>
 
-<main class="main box-border">
+<main class="main box-border flex min-h-full flex-col justify-center gap-2">
 	{#if !results}
 		<Spinner></Spinner>
 		<p>Загрузка теста {slug}...</p>
 	{:else if results.length != 0}
-		<!-- <div class="flex flex-col items-center justify-center gap-2">
-		</div> -->
-		<div class="flex min-h-full flex-col justify-center gap-2">
-			{#each results as result}
-				<div class="w-full rounded-2xl bg-gray-600 shadow">
-					<button
-						class={`flex w-full cursor-pointer items-center justify-between rounded-t-2xl px-4 py-3 transition-colors hover:bg-gray-400 ${openedSessionId != result.sessionId ? 'hover:rounded-b-2xl' : ''}`}
-						onclick={() => toggleSession(result.sessionId)}
+		{#each results as result}
+			<div class="w-full rounded-2xl bg-gray-600 shadow">
+				<button
+					class={`flex w-full cursor-pointer items-center justify-between rounded-t-2xl px-4 py-3 transition-colors hover:bg-gray-400 ${openedSessionId != result.sessionId ? 'hover:rounded-b-2xl' : ''}`}
+					onclick={() => toggleSession(result.sessionId)}
+				>
+					<span class="font-medium text-gray-50">
+						{openedSessionId === result.sessionId
+							? 'Попытка от ' + formatUserLocalDate(result.createdAt)
+							: formatUserLocalDate(result.createdAt)}
+					</span>
+					<svg
+						class={`h-5 w-5 transform text-gray-500 transition-transform ${
+							openedSessionId === result.sessionId ? 'rotate-180' : ''
+						}`}
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
 					>
-						<span class="font-medium text-gray-50">
-							{openedSessionId === result.sessionId
-								? 'Попытка от ' + formatUserLocalDate(result.createdAt)
-								: formatUserLocalDate(result.createdAt)}
-						</span>
-						<svg
-							class={`h-5 w-5 transform text-gray-500 transition-transform ${
-								openedSessionId === result.sessionId ? 'rotate-180' : ''
-							}`}
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke="currentColor"
-						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								d="M19 9l-7 7-7-7"
-							/>
-						</svg>
-					</button>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M19 9l-7 7-7-7"
+						/>
+					</svg>
+				</button>
 
-					{#if openedSessionId === result.sessionId && Comp}
-						<div class="box-border flex flex-col items-center border-t p-2">
-							<Comp
-								testType={slug as keyof TestResultMap}
-								results={result.attempts}
-								meta={'meta' in result ? result['meta'] : undefined}
-							/>
-						</div>
-					{/if}
-				</div>
-			{/each}
-		</div>
+				{#if openedSessionId === result.sessionId && Comp}
+					<div class="box-border flex flex-col items-center border-t p-2">
+						<Comp
+							testType={slug as keyof TestResultMap}
+							results={result.attempts}
+							meta={'meta' in result ? result['meta'] : undefined}
+						/>
+					</div>
+				{/if}
+			</div>
+		{/each}
 	{:else}
 		<h1>Попыток нет</h1>
 	{/if}
