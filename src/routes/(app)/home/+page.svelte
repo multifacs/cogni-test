18c6659041e64b735b48ff4bf3072b42a5784e0f
Roddyck <rodion.ivashchenko2003@gmail.com>
2026-08-20 17:04:30 +0300

    move message out of modal to the page conditionally

--- src/routes/(app)/home/+page.svelte
+++ src/routes/(app)/home/+page.svelte
@@ -11,11 +11,11 @@
 	let diferredInstallEvent: any | null = $state(null);
 	let showInstallButton = $state(true);
 	let showInstallModal = $state(false);
-	let showRunAllModal = $state(false);
+	let undiagnosed = $state(false); // i guess with variable is about me
 
 	onMount(async () => {
 		if (data.hasUnfinishedTests && !data.loggedInAdmin) {
-			showRunAllModal = true;
+			undiagnosed = true;
 		}
 
 		const lfShowInstallButton: boolean | null = await localforage.getItem('showInstallButton');
@@ -79,81 +79,82 @@
 	<h1 class="text-3xl font-bold">Добро пожаловать!</h1>
 </section>
 <main class="main flex flex-col items-center justify-center gap-4">
-	<div class="flex w-full max-w-xs flex-col gap-4">
-		<Button color="green" goto="/tests">🧪 Когнитивный возраст</Button>
-		<Button color="gray" goto="/exercises">📊 Когнитивный тренажёр</Button>
-		<Button color="blue" goto="/materials">📚 Когнитивное долголетие</Button>
-		<Button color="orange" goto="/gto">🏆 ГТО-М</Button>
+	{#if !undiagnosed}
+		<div class="flex w-full max-w-xs flex-col gap-4">
+			<Button color="green" goto="/tests">🧪 Когнитивный возраст</Button>
+			<Button color="gray" goto="/exercises">📊 Когнитивный тренажёр</Button>
+			<Button color="blue" goto="/materials">📚 Когнитивное долголетие</Button>
+			<Button color="orange" goto="/gto">🏆 ГТО-М</Button>
 
-		{#if showInstallButton}
-			<div class="flex w-full max-w-xs flex-col gap-4 text-center">
-				<h3 class="text-lg">Вы также можете установить приложение на своем устройстве</h3>
-				<Button color="green" onclick={handleInstall}>Установить приложение</Button>
-			</div>
-		{/if}
-		{#if showRunAllModal}
-			<Modal bind:showModal={showInstallModal}>
-				{#snippet header()}
-					<h2 class="text-2xl text-white">
-						Пройдите начальную диагностику, чтобы получить доступ ко всем возможностям
-						приложения
-					</h2>
-				{/snippet}
-				<div class="flex flex-col gap-4">
-					<Button color="green" onclick={handleRunAll}>Пройти диагностику</Button>
+			{#if showInstallButton}
+				<div class="flex w-full max-w-xs flex-col gap-4 text-center">
+					<h3 class="text-lg">
+						Вы также можете установить приложение на своем устройстве
+					</h3>
+					<Button color="green" onclick={handleInstall}>Установить приложение</Button>
 				</div>
-			</Modal>
-		{/if}
-		{#if showInstallModal}
-			<Modal bind:showModal={showInstallModal}>
-				{#snippet header()}
+			{/if}
+			{#if showInstallModal}
+				<Modal bind:showModal={showInstallModal}>
+					{#snippet header()}
+						<div class="flex flex-col gap-4">
+							<h2 class="text-2xl text-white">
+								Установка приложения на не chrome-based браузерах
+							</h2>
+							<p class="text-white">Похоже, Вы используете firefox или safari.</p>
+							<p class="text-white">
+								Если Вы используете <b>Safari</b>, то вы можете установить
+								приложение на своем устройстве вручную.
+							</p>
+							<ol class="list-inside list-decimal text-white">
+								<li>
+									<b>Нажмите «Поделиться»</b>: Найдите иконку "Поделиться"
+									(квадрат со стрелкой, смотрящей вверх) внизу или вверху экрана и
+									нажмите на нее.
+								</li>
+								<li>
+									<b>Выберите «На экран «Домой»»:</b> В появившемся меню прокрутите
+									вниз и выберите этот пункт.
+								</li>
+								<li>
+									<b>Подтвердите установку:</b> Задайте имя для ярлыка и нажмите Добавить
+									в правом верхнем углу.
+								</li>
+								<li>
+									<b>Готово:</b> Иконка PWA появится на главном экране, и при нажатии
+									он будет запускаться как отдельное приложение.
+								</li>
+							</ol>
+							<p class="text-white">
+								<b>Firefox</b> не поддерживает установку pwa приложений. В этом случае
+								воспользуйтесь другим браузером.
+							</p>
+						</div>
+					{/snippet}
 					<div class="flex flex-col gap-4">
-						<h2 class="text-2xl text-white">
-							Установка приложения на не chrome-based браузерах
-						</h2>
-						<p class="text-white">Похоже, Вы используете firefox или safari.</p>
-						<p class="text-white">
-							Если Вы используете <b>Safari</b>, то вы можете установить приложение на
-							своем устройстве вручную.
-						</p>
-						<ol class="list-inside list-decimal text-white">
-							<li>
-								<b>Нажмите «Поделиться»</b>: Найдите иконку "Поделиться" (квадрат со
-								стрелкой, смотрящей вверх) внизу или вверху экрана и нажмите на нее.
-							</li>
-							<li>
-								<b>Выберите «На экран «Домой»»:</b> В появившемся меню прокрутите вниз
-								и выберите этот пункт.
-							</li>
-							<li>
-								<b>Подтвердите установку:</b> Задайте имя для ярлыка и нажмите Добавить
-								в правом верхнем углу.
-							</li>
-							<li>
-								<b>Готово:</b> Иконка PWA появится на главном экране, и при нажатии он
-								будет запускаться как отдельное приложение.
-							</li>
-						</ol>
-						<p class="text-white">
-							<b>Firefox</b> не поддерживает установку pwa приложений. В этом случае воспользуйтесь
-							другим браузером.
-						</p>
+						<Button color="green" onclick={() => (showInstallModal = false)}
+							>Понятно</Button
+						>
+						<Button
+							color="red"
+							onclick={() => {
+								localforage.setItem('showInstallButton', false);
+								showInstallModal = false;
+							}}>Больше не показывать</Button
+						>
 					</div>
-				{/snippet}
-				<div class="flex flex-col gap-4">
-					<Button color="green" onclick={() => (showInstallModal = false)}>Понятно</Button
-					>
-					<Button
-						color="red"
-						onclick={() => {
-							localforage.setItem('showInstallButton', false);
-							showInstallModal = false;
-						}}>Больше не показывать</Button
-					>
-				</div>
-			</Modal>
-		{/if}
-	</div>
+				</Modal>
+			{/if}
+		</div>
+	{:else}
+		<div class="flex w-full max-w-xs flex-col gap-4">
+			<h2 class="text-2xl text-white">
+				Пройдите начальную диагностику, чтобы получить доступ ко всем возможностям
+				приложения
+			</h2>
+			<Button color="green" onclick={handleRunAll}>Пройти диагностику</Button>
+		</div>
+	{/if}
 </main>
 <section class="low-content flex items-center justify-center">
 	<p class="max-w-md text-center text-lg max-md:text-sm">Выберите, с чего начать.</p>
