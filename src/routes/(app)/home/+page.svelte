46a40de9292af76e521136cc23256b92ae5490b0
shelepin.n <shelepin.n@selectel.ru>
2026-09-02 01:47:57 +0300

    fixed home page

--- src/routes/(app)/home/+page.svelte
+++ src/routes/(app)/home/+page.svelte
@@ -13,8 +13,6 @@
 	let showInstallModal = $state(false);
 	let undiagnosed = $state(false); // i guess with variable is about me
 
-	let { data } = $props();
-
 	onMount(async () => {
 		if (data.hasUnfinishedTests && !data.loggedInAdmin) {
 			undiagnosed = true;
@@ -87,7 +85,7 @@
 			<Button color="gray" goto="/exercises">📊 Когнитивный тренажёр</Button>
 			<Button color="blue" goto="/materials">📚 Когнитивное долголетие</Button>
 			<Button color="orange" goto="/gto">🏆 ГТО-М</Button>
-      <Button color="pink" goto="/metrics">Метрики WIP</Button>
+			<Button color="pink" goto="/metrics">Метрики WIP</Button>
 
 			{#if showInstallButton}
 				<div class="flex w-full max-w-xs flex-col gap-4 text-center">
@@ -148,26 +146,26 @@
 					</div>
 				</Modal>
 			{/if}
-    <div class="flex w-full max-w-xs flex-col gap-4 text-center">
-    <h3 class="text-lg">Рекомендации</h3>
-		</div>
-		{#if !data.hasData}
-			<span>Сначала пройдите тесты и упражнения, чтобы получить рекомендации.</span>
-		{:else}
-			<div class="flex w-full flex-col gap-3">
-				{#each data.recommendations as { name, title, path, img }}
-					<a
-						href={path}
-						class="flex items-center justify-between rounded-2xl bg-gray-600 p-3 shadow transition hover:bg-gray-100 hover:text-black"
-					>
-						<div class="flex flex-col gap-1">
-							<span class="text-lg">{title}</span>
-						</div>
-						<img src={img} alt={name} class="h-14 w-14 rounded-xl bg-white" />
-					</a>
-				{/each}
+			<div class="flex w-full max-w-xs flex-col gap-4 text-center">
+				<h3 class="text-lg">Рекомендации</h3>
 			</div>
-		{/if}
+			{#if !data.hasData}
+				<span>Сначала пройдите тесты и упражнения, чтобы получить рекомендации.</span>
+			{:else}
+				<div class="flex w-full flex-col gap-3">
+					{#each data.recommendations as { name, title, path, img }}
+						<a
+							href={path}
+							class="flex items-center justify-between rounded-2xl bg-gray-600 p-3 shadow transition hover:bg-gray-100 hover:text-black"
+						>
+							<div class="flex flex-col gap-1">
+								<span class="text-lg">{title}</span>
+							</div>
+							<img src={img} alt={name} class="h-14 w-14 rounded-xl bg-white" />
+						</a>
+					{/each}
+				</div>
+			{/if}
 		</div>
 	{:else}
 		<div class="flex w-full max-w-xs flex-col gap-4">
