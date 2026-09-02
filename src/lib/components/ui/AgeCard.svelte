84b1145aeb3cda9304344555e53a367f1c78d30f
shelepin.n <shelepin.n@selectel.ru>
2026-09-02 13:10:46 +0300

    Refactor AgeCard runes

--- src/lib/components/ui/AgeCard.svelte
+++ src/lib/components/ui/AgeCard.svelte
@@ -2,7 +2,7 @@
 	let { age, realAge } = $props();
 	let displayAge = $derived(age === null || age === undefined ? null : Math.round(age));
 
-	let trackerStatus = $derived(() => {
+	let trackerStatus = $derived.by(() => {
 		if (displayAge === null || realAge === null || realAge === undefined) {
 			return 'unknown';
 		}
@@ -11,23 +11,21 @@
 		return 'worse';
 	});
 
-	let statusText = $derived(() => {
-		const status = trackerStatus();
-		if (status === 'unknown') {
+	let statusText = $derived.by(() => {
+		if (trackerStatus === 'unknown') {
 			return 'Пройдите тесты, чтобы узнать свой возраст';
 		}
-		if (status === 'better') {
+		if (trackerStatus === 'better') {
 			return `Поздравляем! Ваш когнитивный возраст моложе реального!`;
 		}
-		if (status === 'equal') {
+		if (trackerStatus === 'equal') {
 			return 'Отлично! Ваш когнитивный возраст совпадает с реальным!';
 		}
 		return `Регулярные тренировки помогут улучшить результат`;
 	});
 
-	let trackerImage = $derived(() => {
-		const status = trackerStatus();
-		switch (status) {
+	let trackerImage = $derived.by(() => {
+		switch (trackerStatus) {
 			case 'better':
 				return '/tracker/tracker_better.svg';
 			case 'equal':
@@ -46,8 +44,8 @@
 		<h1>{displayAge ?? '??'} лет</h1>
 	</div>
 
-	<h4>{statusText()}</h4>
-	{#if trackerImage()}<img class="img" src={trackerImage()} alt="Tracker" />{/if}
+	<h4>{statusText}</h4>
+	{#if trackerImage}<img class="img" src={trackerImage} alt="Tracker" />{/if}
 </div>
 
 <style>
