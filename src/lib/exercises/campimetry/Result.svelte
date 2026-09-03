1356c4eda3d848fc80846ba7acca714a0da0be67
shelepin.n <shelepin.n@selectel.ru>
2026-09-03 23:04:23 +0300

    chore: removing svelte warns

--- src/lib/exercises/campimetry/Result.svelte
+++ src/lib/exercises/campimetry/Result.svelte
@@ -3,19 +3,13 @@
 	import type { ExerciseResults } from '$lib/exercises/types';
 	import ResultsChart from '$lib/tests/campimetry/ResultsChart.svelte';
 
-	let {
-		results,
-		exerciseType,
-		meta
-	}: {
-		results: ExerciseResults;
-		exerciseType?: string;
-		meta?: string[];
-	} = $props();
+	let { results }: { results: ExerciseResults } = $props();
 
-	const allTime = Math.round(results.reduce((a: number, b: any) => a + b.time, 0) / 1000);
-	const avg = Math.round(
-		results.reduce((a: number, b: any) => a + b.time, 0) / results.length / 1000
+	const allTime = $derived(
+		Math.round(results.reduce((a: number, b: any) => a + b.time, 0) / 1000)
+	);
+	const avg = $derived(
+		Math.round(results.reduce((a: number, b: any) => a + b.time, 0) / results.length / 1000)
 	);
 </script>
 
